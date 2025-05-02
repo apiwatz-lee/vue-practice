a1d44c465d5a871b202270947f38f816352835b0
Apiwat Lee <apiwatz.lee@gmail.com>
2025-05-02 16:52:37 +0700

    feat:ref computesd onmounted watch

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,131 @@
 <script setup>
-import { reactive } from 'vue';
-const genderList = ['ชาย', 'หญิง', 'ไม่ระบุ'];
-const interestList = ['กีฬา', 'ดนตรี', 'การศึกษา', 'การท่องเที่ยว'];
+import { ref, computed, onMounted, watch } from 'vue';
 
-const submitForm = () => {
-  console.log(formData);
+const isLoading = ref(false);
+const isUpdated = ref(false);
+const isValid = ref(true);
+const firstname = ref('');
+const lastname = ref('');
+const email = ref('');
+const errors = ref({});
+
+const fullname = computed(() => {
+  return `${firstname.value} ${lastname.value}`;
+});
+
+const updateProfile = async () => {
+  if (!isValid.value) {
+    alert('Please fix the errors before submitting');
+    return;
+  }
+  isLoading.value = true;
+  try {
+    // Simulate an API call
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    isUpdated.value = true;
+  } catch (error) {
+    alert('Error updating profile');
+  } finally {
+    isLoading.value = false;
+  }
 };
 
-const formData = reactive({
-  firstName: '',
-  lastName: '',
-  description: '',
-  gender: '',
-  interest: [],
+const validateName = (name) => {
+  const re = /\d/;
+  return !re.test(name);
+};
+
+const validateEmail = (email) => {
+  return email.includes('@');
+};
+
+watch([firstname, lastname, email], () => {
+  isValid.value = true;
+  isUpdated.value = false;
+  errors.value = {};
+
+  if (!validateName(firstname.value)) {
+    isValid.value = false;
+    errors.value.firstname = 'Firstname should not contain numbers';
+  }
+
+  if (!validateName(lastname.value)) {
+    isValid.value = false;
+    errors.value.lastname = 'Lastname should not contain numbers';
+  }
+
+  if (!validateEmail(email.value)) {
+    isValid.value = false;
+    errors.value.email = 'Email should contain @';
+  }
+});
+
+onMounted(() => {
+  firstname.value = 'John ';
+  lastname.value = 'Doe';
+  email.value = 'johndoe@gmail.com';
 });
 </script>
 
 <template>
-  <div>
-    <label for="firstName">firstName</label>
-    <input type="text" name="firstName" v-model="formData.firstName" />
-  </div>
-
-  <div>
-    <label for="lastName">lastName</label>
-    <input type="text" name="lastName" v-model="formData.lastName" />
-  </div>
-
-  <div>
-    <label for="description">description</label>
-    <textarea
-      name="description"
-      id="description"
-      v-model="formData.description"
-    ></textarea>
-  </div>
-
-  <div>
-    <label for="gender">gender</label>
-
-    <div v-for="gender in genderList" :key="gender">
-      <input
-        type="radio"
-        :name="gender"
-        :value="gender"
-        v-model="formData.gender"
-      />
-      <label :for="gender">{{ gender }}</label>
+  <div class="container">
+    <div>
+      <div>Fullname: {{ fullname }}</div>
+      <div>email: {{ email }}</div>
     </div>
-  </div>
-
-  <div>
-    <label for="interest">interest</label>
-    <div v-for="interest in interestList" :key="interest">
-      <input type="checkbox" :value="interest" v-model="formData.interest" />
-      <label :for="interest">{{ interest }}</label>
+    <div>
+      <div>Firstname:</div>
+      <input type="text" v-model="firstname" />
+      <div class="errors">{{ errors.firstname }}</div>
     </div>
-  </div>
 
-  <button type="submit" @click="submitForm()">ส่ง form</button>
+    <div>
+      <div>Lastname:</div>
+      <input type="text" v-model="lastname" />
+      <div class="errors">{{ errors.lastname }}</div>
+    </div>
+
+    <div>
+      <div>Email:</div>
+      <input type="text" v-model="email" />
+      <div class="errors">{{ errors.email }}</div>
+    </div>
+
+    <div v-if="isLoading">Loading ...</div>
+
+    <button :disabled="!isValid" @click="updateProfile()">
+      Update profile
+    </button>
+    <div v-if="isUpdated">updated successfully</div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+.container > div {
+  width: 100%;
+}
+
+input {
+  width: 100%;
+}
+
+button {
+  width: 100%;
+  height: 24px;
+  margin-top: 10px;
+}
+
+.errors {
+  color: red;
+  font-size: 12px;
+}
+</style>
